f2da9b85540b2633ff3123e1daeb95d39ccc22ab
dilpreet2012 <dilkill2000@gmail.com>
2024-09-27 13:30:31 +0530

    data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,15 @@
+let name="none"
+let address='badh mandi'
+let a="none"
+let age=29
+let salary=99.897
+let happiness=true;
+let rts;
+let io=null;
+let xyz=2345n;
+let uvw=Symbol("princess")
+let o=Symbol("princess")
+console.log(name,address,age,salary,happiness,rts,io,xyz,uvw)
+console.log(typeof name, typeof address, typeof age, typeof salary, typeof happiness,typeof rts,typeof io,typeof xyz,typeof uvw);
+console.log(name===a)
+console.log(uvw===0)
