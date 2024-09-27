3697427f4fc3671b148afd22202b142295b53b5d
dilpreet2012 <dilkill2000@gmail.com>
2024-09-27 12:46:32 +0530

    variable declaration(let,var,const)

---
+++ 01_basics.js
@@ -0,0 +1,16 @@
+//use of let,var and const
+
+
+const accountid=14453
+let account_Email="hitesh@google.com"
+var account_password="12345"
+accountCity="Jaipur"
+account_Email="hc@hc.com"
+account_password="2121"
+accountCity="Bengaluru"
+
+if(accountCity==="Bengaluru")
+{
+    account_Email="ic@ic.com"
+}
+console.table([accountid,account_Email,account_password,accountCity])
