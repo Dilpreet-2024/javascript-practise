9689f3c1e6e95b8d6254b0a0ed7aed17cf313cc0
dilpreet2012 <dilkill2000@gmail.com>
2024-09-27 15:52:41 +0530

    string comparsion operator

---
+++ 05_comparsion operator.js
@@ -0,0 +1,10 @@
+//int this section we well learn about different type of comparsion operators in javascript
+
+/*
+NOTE:-
+NEver compare two different type of data type
+*/
+console.log(2>2)
+console.log(2=="2")
+console.log(2==="2")
+console.log(2>"muski")
