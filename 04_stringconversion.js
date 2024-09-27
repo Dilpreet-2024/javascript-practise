84244360f160a1e13365f71e21208e3787a1fd6a
dilpreet2012 <dilkill2000@gmail.com>
2024-09-27 15:16:52 +0530

    addition of string and number

---
+++ 04_stringconversion.js
@@ -0,0 +1,13 @@
+//int this section we will add number and a string
+// we wil get to know about the problems while adding string and a number
+
+
+
+//whenever we add a number and a string , it will automatically be converted to string]
+console.log(2+2);
+console.log(1+"2");
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+"2"+2)
+console.log(1+2+"2")
