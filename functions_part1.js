a71a6890fad662a0b21bb5bad664c3500f9524ee
dilpreet2012 <dilkill2000@gmail.com>
2024-10-08 11:35:29 +0530

    basics of functions

---
+++ functions_part1.js
@@ -0,0 +1,26 @@
+//In this part we will study about functions in javascript
+function print()
+{
+    console.log("Hello world")
+}
+print()
+function pr()
+{
+    return "Hello world"
+}
+let value=pr()
+console.log(value)
+
+
+function add(n1,n2){
+return n1+n2; 
+}
+let ans=add(null,5)
+console.log(ans)
+
+
+function details(name)
+{
+    return `Welcome this site is designed by ${name}`
+}
+console.log(details("khushi"))
