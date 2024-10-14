ba8f130c80d31c4e201125775d86cc20428dec0b
dilpreet2012 <dilkill2000@gmail.com>
2024-10-14 10:37:12 +0530

    arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,32 @@
+// in this part we will study about arrow functions in javascript
+// arrow functions are used to define small functions in a more concise way
+let obj={
+    username:"abc",
+    mobile:4030,
+    details:function(){
+        console.log(`Hello my name is ${this.username}`)
+    }
+}
+obj.details();
+obj.username="xyz";
+obj.details();
+
+//normal function
+const add=function(x,y)
+{
+    return x+y;
+}
+console.log(add(1,2));
+//arrow functions with explicit return
+const adds=(x,y)=>{
+return x+y;
+}
+console.log(adds(1,4));
+
+//arrow functions with implicit return
+const adder=(x,y)=>(x+y)
+console.log(adder(5,3));
+
+//returning objects with implicit return
+const print=()=>({a:"abc",b:"xyz",c:false})
+console.log(print());
