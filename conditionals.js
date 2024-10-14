84704eced06eb9f69621da804871548701844f50
dilpreet2012 <dilkill2000@gmail.com>
2024-10-14 12:33:32 +0530

    conditional statements

---
+++ conditionals.js
@@ -0,0 +1,32 @@
+if(2>4)
+    console.log("eecuted")
+else
+console.log("not executed");
+
+//switch statement
+let month="January";
+switch(month)
+{
+    case "January":{
+        console.log("January");
+        break;
+    }
+    case "Feburary":{
+        console.log("Feburary");
+        break;
+    }
+    case "March":{
+        console.log("March");
+        break;
+    }
+    case "April":{
+        console.log("April");
+        break;
+    }
+    case "May":{
+        console.log("May");
+        break;
+    }
+    default:
+        console.log("Value not in range");
+}
