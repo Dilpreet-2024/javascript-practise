65ac874b447055e09340b48f58df320bb9db121d
dilpreet2012 <dilkill2000@gmail.com>
2024-10-14 10:55:25 +0530

    iife

--- IIFE.js
+++ IIFE.js
@@ -1,3 +1,5 @@
 //IIFE=Immediately Invoked Function Expression
 //(function definition)(function execution)
 (function(){console.log("hello world") })();
+(function(a,b){console.log(a+b)})(4,5);
+((a,b)=>{console.log(a+b)})(4,3);
