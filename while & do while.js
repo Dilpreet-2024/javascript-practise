a1a742e596b4256cd27f177b0da36d1af29f4d0e
dilpreet2012 <dilkill2000@gmail.com>
2024-10-17 09:33:32 +0530

    while and do while loops in js

---
+++ while & do while.js
@@ -0,0 +1,13 @@
+// in this part we will learn about while and do while loops in js
+let i=1;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+}
+//do while loops in js
+let x=1;
+do{
+    console.log(x);
+    x++;
+}while(x<=10);
